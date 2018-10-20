e672093a91310b7269c4f2768fea5d5652eb210a
Andrej Pistek <andrej.pistek@outlook.com>
2018-10-20 15:16:13 +0200

    Final.

--- test/Date_test.cpp
+++ test/Date_test.cpp
@@ -8,8 +8,3 @@ TEST(Test,IncrementTest){
     date.increment_day(4);
     ASSERT_EQ(date.str(),"03-11-2020");
 }
-
-TEST(Test, OutputTest){
-    Date date=Date{6,8,2018};
-    ASSERT_EQ(date.str(),"06-08-2018");
-}
