e440121141d7e268d88b629aefff52176bdfdad2
Andrej Pistek <andrej.pistek@outlook.com>
2018-10-20 15:22:49 +0200

    Final.

--- test/Date_test.cpp
+++ test/Date_test.cpp
@@ -3,7 +3,7 @@
 #include "../Date/Date.h"
 #include "../Date/Date.cpp"
 
-TEST(Test,IncrementTest){
+TEST(Test,Date_test){
     Date date=Date{30,10,2020};
     date.increment_day(4);
     ASSERT_EQ(date.str(),"03-11-2020");
